84dd8e3206a8a24528c79caed52a81ce46f1de50
Mariana <nanomarii10@gmail.com>
2022-12-13 10:41:35 -0300

    Requisito 9 - concluído

--- src/router/categoryRoute.js
+++ src/router/categoryRoute.js
@@ -6,5 +6,6 @@ const { validateToken } = require('../middlewares/validateUser');
 const router = Router();
 
 router.post('/', validateToken, validateNameCategory, categoryController.addNewCategory);
+router.get('/', validateToken, categoryController.getAllCategories);
 
 module.exports = router;
--- src/services/categoryService .js
+++ src/services/categoryService .js
@@ -1,14 +1,16 @@
 const { Category } = require('../models');
-// const { createToken } = require('../auth/validateJwt');
 
 const addNewCategory = async (category) => {
   const newCategory = await Category.create(category);
-  // const { password: _password, ...dataWithoutPassword } = newUser.dataValues;
-  // const token = createToken(dataWithoutPassword);
-
     return newCategory;
+};
+
+const getAllCategories = async () => {
+  const allCategories = await Category.findAll();
+    return allCategories;
 };
 
 module.exports = {
   addNewCategory,
+  getAllCategories,
 };
